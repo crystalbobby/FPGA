ece3cfa0d190931c8b47fa383461377afde3180e
crystalbobby <crystalbobby@163.com>
2018-03-07 15:58:37 +0800

    add batch file to do auto simulation

---
+++ ddr3/modelsim_run.bat
@@ -0,0 +1,3 @@
+cd ./ipcore_dir/DDR3/example_design/sim/
+
+C:\modeltech64_10.4\win64\modelsim.exe -do  sim.do
---
+++ xaui_example/modelsim_run.bat
@@ -0,0 +1,3 @@
+cd ./ipcore_dir/xaui_wrapper/simulation/functional/
+
+C:\modeltech64_10.4\win64\modelsim.exe -do  simulate_mti.do
